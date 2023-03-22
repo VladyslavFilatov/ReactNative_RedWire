2665d0a31f5d404d2258d2074b7e289b2b5a629a
Vladyslav Filatov <107763268+VladyslavFilatov@users.noreply.github.com>
2023-03-22 19:05:09 +0000

    Update index.js

--- RedWire/src/firebase/index.js
+++ RedWire/src/firebase/index.js
@@ -5,31 +5,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc } from "firebase/firestore";
 
 const config = {
-    apiKey: "AIzaSyDXM69_Gu5fujPsO71epKV9foVIyufUYZg",
-    authDomain: "redwire-89a48.firebaseapp.com",
-    projectId: "redwire-89a48",
-    storageBucket: "redwire-89a48.appspot.com",
-    messagingSenderId: "626267553389",
-    appId: "1:626267553389:web:e30440fa3ab02dff91c964",
-    measurementId: "G-6DE4VQX5L6"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 }
-
-//firebase.initializeApp(config);
-
-/* const firebaseApp = initializeApp(config);
-const DB = getFirestore(firebaseApp);
-const usersCollection = collection(DB, 'users');
-
-export {
-  firebaseApp,
-  usersCollection
-} */
-
-// Importing the default export
-
-/* if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-  } */
   
 
 const firebaseApp = initializeApp(config);
